c02b2b328861282ab5571a7c90104e575c24efc1
byprogram <121013897+byprogram@users.noreply.github.com>
2023-03-27 05:07:13 +0800

    Update conf.js

--- config/conf.js
+++ config/conf.js
@@ -7,5 +7,5 @@ module.exports = {
         database : 'shuangxiang', //mysql数据库
     }),
     token: '5935850388:AAHEssKugiFBhFDxbBP1hOtMq9S7Bm2X78c', //机器人的token
-    adminid:-1001971441758 //管理群的id 使用机器人@qunid_bot获取id
+    adminid:-1001971441758 //管理员或管理群的id    使用机器人 @qunid_bot 获取id
 }
